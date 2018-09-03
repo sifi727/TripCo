2f9e66805a9c3200bf6fc2762e3ea39570b29ce3
Jerrel Siler <42549678+sifi727@users.noreply.github.com>
2018-09-03 14:09:51 -0600

    Update Destination.java
    OK, I think I fixed it.  The compiler was complaining since I didn't put parenthesis  in one of the areas.

--- server/src/main/java/com/tripco/t07/planner/Destination.java
+++ server/src/main/java/com/tripco/t07/planner/Destination.java
@@ -93,6 +93,6 @@ public class Distance
 
         //The 0.5 at the end of the next line below will round up the answer 
         //(Java truncates decimals when converting from double to int).
-        this.distance = Math.atan(Math.sqrt(Math.pow(Math.cos(destination_latitude) + Math.sin(origin_longitude - destination_longitude), 2) + Math.pow((Math.cos(origin_latitude) * Math.sin(destination_latitude)) - (Math.sin(origin_latitude) * Math.cos(destination_latitude) * Math.cos(origin_longitude - destination_longitude), 2)))/((Math.sin(origin_latitude) * Math.sin(destination_latitude)) + (Math.cos(origin_latitude) * Math.cos(destination_latitude) * Math.cos(origin_longitude - destination_longitude)))) + 0.5;
+        this.distance = Math.atan(Math.sqrt(Math.pow(Math.cos(destination_latitude) + Math.sin(origin_longitude - destination_longitude), 2) + Math.pow((Math.cos(origin_latitude) * Math.sin(destination_latitude)) - ((Math.sin(origin_latitude) * Math.cos(destination_latitude) * Math.cos(origin_longitude - destination_longitude)), 2)))/((Math.sin(origin_latitude) * Math.sin(destination_latitude)) + (Math.cos(origin_latitude) * Math.cos(destination_latitude) * Math.cos(origin_longitude - destination_longitude))) + 0.5;
     }
 }
