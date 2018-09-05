94a6569d90d52106b7a78aa8895132b964c2601d
Jerrel Siler <42549678+sifi727@users.noreply.github.com>
2018-09-05 11:29:47 -0600

    Update Distance.java
    Change the visibility of class variables from private to public.  Eliminated the getters and setters since now the variables are visible to the public.  Change line 46, eliminate Math.floor and replaced it with the Math.round function.  Also removed the 0.5 from the formula.

--- server/src/main/java/com/tripco/t07/planner/Distance.java
+++ server/src/main/java/com/tripco/t07/planner/Distance.java
@@ -3,79 +3,14 @@ package com.tripco.t07.planner;
 public class Distance
 {
     //The variables in this class should match the REST API definition.
-    private String type;
-    private int version;
-    private Place origin;
-    private Place destination;
-    private String units;
-    private int distance;
-
-    //class getters (or accessors)
-    public String getType()
-    {
-        return this.type;
-    }
-
-    public int getVersion()
-    {
-        return this.version;
-    }
-
-    public Place getOrigin()
-    {
-        return this.origin;
-    }
-
-    public Place getDestination()
-    {
-        return this.destination;
-    }
-
-    public String getUnits()
-    {
-        return this.units;
-    }
-
-    public int getDistance()
-    {
-        return this.distance;
-    }
-
-
-    //class setters (or mutators)
-    public void setType(String type)
-    {
-        this.type = type;
-    }
-
-    public void setVersion(int version)
-    {
-        this.version = version;
-    }
-
-    public void setOrigin(Place origin)
-    {
-        this.origin = origin;
-    }
-
-    public void setDestination(Place destination)
-    {
-        this.destination = destination;
-    }
-
-    public void setUnits(String units)
-    {
-        this.units = units;
-    }
-
-    public void setDistance(int distance)
-    {
-        this.distance = distance;
-    }
-
-
+    public String type;
+    public int version;
+    public Place origin;
+    public Place destination;
+    public String units;
+    public int distance;
+ 
     //class methods
-
     //This calls a helper method haversineVincenty().
     public void calculateTotalDistance()
     {
@@ -107,7 +42,7 @@ public class Distance
         double right_denominator_component = Math.cos(origin_latitude) * Math.cos(destination_latitude) * Math.cos(delta_lambda);
         double denominator = left_denominator_component + right_denominator_component;
 
-        //The 0.5 at the end of the next line below will round up the answer when converting back into the distance int variable.
-        this.distance = (int)(Math.floor((radius * Math.atan2(numerator, denominator)) + 0.5));
+        //Calculate the distance between the origin and the destination. 
+        this.distance = (int)(Math.round(radius * Math.atan2(numerator, denominator)));
     }
 }
