b4dc74e53e062e756ab0b52727e2a6a1e106ef02
davidjump <unstuck.git@davidjump.com>
2018-09-24 14:49:56 -0600

    Added tests to test custom units and different maps. Link to task #118

--- server/src/test/java/com/tripco/t07/planner/TestTrip.java
+++ server/src/test/java/com/tripco/t07/planner/TestTrip.java
@@ -20,6 +20,73 @@ import static org.junit.Assert.*;
 public class TestTrip {
 
   Trip trip;
+  //Helper Class
+  private String getJsonThreeCityWithMiles(){
+    return " {\"options\" : {\n" +
+        "\"units\":\"miles\"\n" +
+        "},\"places\"    : [\n" +
+        "  {\"id\":\"dnvr\", \"name\":\"Denver\", \"latitude\":39.7392, \"longitude\":-104.9903},\n"
+        +
+        "  {\"id\":\"bldr\", \"name\":\"Boulder\", \"latitude\":40.01499, \"longitude\":-105.27055},\n"
+        +
+        "  {\"id\":\"foco\", \"name\":\"Fort Collins\", \"latitude\":40.58258, \"longitude\":-105.084419}\n"
+        +
+        "  ]}";
+  }
+  private String getJsonFourStateBorderWithTitle(){
+    return "{\n"
+        + "  \"version\"   : 2,\n"
+        + "  \"type\"      : \"trip\",\n"
+        + "  \"title\"     : \"Shopping loop\",\n"
+        + "    \"options\" : { \n"
+        + "  \"units\"        : \"miles\"\n"
+        + "},\n"
+        + "  \"places\"    : [\n"
+        + "  {\"id\":\"AZ-CO\", \"name\":\"B1\", \"latitude\":36.999, \"longitude\":-109.044},\n"
+        + "  {\"id\":\"CO-KS\", \"name\":\"B2\", \"latitude\":36.993, \"longitude\":-102.041},\n"
+        + "  {\"id\":\"CO-NE\", \"name\":\"B3\", \"latitude\":40.003, \"longitude\":-102.051},\n"
+        + "   {\"id\":\"CO-UT\", \"name\":\"B4\", \"latitude\":41.000, \"longitude\":-109.044}\n"
+        + "  ]\n"
+        + "}";
+  }
+  private String getJsonFourStateCornerBorderWithCustomUnits(){
+    return "{\n"
+        + "\t\"type\": \"trip\",\n"
+        + "\t\"version\": 2,\n"
+        + "\t\"title\": \"Four Corners of CO State\",\n"
+        + "\t\"options\": {\n"
+        + "\t\t\"units\": \"user defined\",\n"
+        + "\t\t\"unitName\": \"accurate miles\",\n"
+        + "\t\t\"unitRadius\": 3958.7613\n"
+        + "\t},\n"
+        + "\t\"places\": [{\n"
+        + "\t\t\t\"id\": \"AZ-CO\",\n"
+        + "\t\t\t\"name\": \"B1\",\n"
+        + "\t\t\t\"latitude\": 36.999,\n"
+        + "\t\t\t\"longitude\": -109.044\n"
+        + "\t\t},\n"
+        + "\t\t{\n"
+        + "\t\t\t\"id\": \"CO-KS\",\n"
+        + "\t\t\t\"name\": \"B2\",\n"
+        + "\t\t\t\"latitude\": 36.993,\n"
+        + "\t\t\t\"longitude\": -102.041\n"
+        + "\t\t},\n"
+        + "\t\t{\n"
+        + "\t\t\t\"id\": \"CO-WY\",\n"
+        + "\t\t\t\"name\": \"B3\",\n"
+        + "\t\t\t\"latitude\": 41,\n"
+        + "\t\t\t\"longitude\": -102.041\n"
+        + "\t\t},\n"
+        + "\t\t{\n"
+        + "\t\t\t\"id\": \"CO-UT\",\n"
+        + "\t\t\t\"name\": \"B4\",\n"
+        + "\t\t\t\"latitude\": 41.000,\n"
+        + "\t\t\t\"longitude\": -109.044\n"
+        + "\t\t}\n"
+        + "\t]\n"
+        + "}";
+  }
+
 
   // Setup to be done before every test in TestPlan
   @Before
@@ -47,16 +114,7 @@ public class TestTrip {
   @Test
   public void testDistancesWithPlaces() {
     trip.places = new ArrayList<>();
-    String placesJson = " {\"options\" : {\n" +
-        "\"units\":\"miles\"\n" +
-        "},\"places\"    : [\n" +
-        "  {\"id\":\"dnvr\", \"name\":\"Denver\", \"latitude\":39.7392, \"longitude\":-104.9903},\n"
-        +
-        "  {\"id\":\"bldr\", \"name\":\"Boulder\", \"latitude\":40.01499, \"longitude\":-105.27055},\n"
-        +
-        "  {\"id\":\"foco\", \"name\":\"Fort Collins\", \"latitude\":40.58258, \"longitude\":-105.084419}\n"
-        +
-        "  ]}";
+    String placesJson = getJsonThreeCityWithMiles();
 
     Gson gson = new Gson();
     trip = gson.fromJson(placesJson, Trip.class);
@@ -98,18 +156,40 @@ public class TestTrip {
     assertEquals(trip.map,stringBuilder.toString());
   }
 
+  @Test public void testWithPlacesAndTitleAndCustomUnitsGetSvgPath()
+  {
+    trip.places =  new ArrayList<>();
+    String placesJson = getJsonFourStateCornerBorderWithCustomUnits();
+    Gson gson = new Gson();
+    trip = gson.fromJson(placesJson, Trip.class);
+    String svgPathActual=trip.getSvgPath();
+    assertEquals("<path\n"
+        + "d= \"M 36.406863,747.665719 L 1032.333652,748.733558 L 1032.333652,35.594655 L 36.406863,35.594655 z\"\n"
+        + "style=\"fill:none; fill-rule:evenodd;stroke:green;stroke-width:3.62829995;stroke-linejoin:round;stroke-miterlimit:3.8636899\"\n"
+        + "\tid=\"path-Four Corners of CO State\"\n"
+        + "\t\t\t/>\n",svgPathActual);
+
+
+      }
+  @Test public void testWithPlacesAndTitleGetSvgPath()
+  {
+    trip.places =  new ArrayList<>();
+    String placesJson = getJsonFourStateBorderWithTitle();
+    Gson gson = new Gson();
+    trip = gson.fromJson(placesJson, Trip.class);
+    String svgPathActual=trip.getSvgPath();
+    assertEquals("<path\n"
+        + "d= \"M 36.406863,747.665719 L 1032.333652,748.733558 L 1030.911509,213.034007 L 36.406863,35.594655 z\"\n"
+        + "style=\"fill:none; fill-rule:evenodd;stroke:green;stroke-width:3.62829995;stroke-linejoin:round;stroke-miterlimit:3.8636899\"\n"
+        + "\tid=\"path-Shopping loop\"\n"
+        + "\t\t\t/>\n",svgPathActual);
+
+
+  }
+
   @Test public void testWithPlacesNoTitleMap(){
     trip.places = new ArrayList<>();
-    String placesJson = " {\"options\" : {\n" +
-        "\"units\":\"miles\"\n" +
-        "},\"places\"    : [\n" +
-        "  {\"id\":\"dnvr\", \"name\":\"Denver\", \"latitude\":39.7392, \"longitude\":-104.9903},\n"
-        +
-        "  {\"id\":\"bldr\", \"name\":\"Boulder\", \"latitude\":40.01499, \"longitude\":-105.27055},\n"
-        +
-        "  {\"id\":\"foco\", \"name\":\"Fort Collins\", \"latitude\":40.58258, \"longitude\":-105.084419}\n"
-        +
-        "  ]}";
+    String placesJson = getJsonThreeCityWithMiles();
 
     Gson gson = new Gson();
     trip = gson.fromJson(placesJson, Trip.class);
