c451e419fed734fb865f5bbbb8776ee365598e97
jsiler <j65536$!>
2018-09-24 16:56:23 -0600

    Code not working when update button is clicked but am pushing to git so others can help with it.

--- client/src/components/Application/UploadTffi.js
+++ client/src/components/Application/UploadTffi.js
@@ -11,9 +11,27 @@ class UploadTffi extends Component{
     }
 
     tffi(){
-        let tffi = document.getElementById('tffi');
-        tffi.type = "submit";
-        this.props.updateTffiObject(tffi);
+        console.log("Starting to submit");
+       //console.log(result);
+      //  let tffi = document.getElementById('tffi');
+        //hard codind results to see if we can call props.updateTiffiObject but it is not working.
+       let tffidoc = "{\n" +
+            " \"type\" : \"trip\",\n" +
+            " \"version\" : 2,\n" +
+            " \"title\" : \"Shopping loop\",\n" +
+            " \"options\" : {\n" +
+            "    \"units\":\"miles\"\n" +
+            "    },\n" +
+            " \"places\" : [\n" +
+            "    {\"id\":\"dnvr\", \"name\":\"Denver\", \"latitude\":39.7392, \"longitude\":-104.9903}, \n" +
+            "    {\"id\":\"bldr\", \"name\":\"Boulder\", \"latitude\":40.01499, \"longitude\":-105.27055}, \n" +
+            "    {\"id\":\"foco\", \"name\":\"Fort Collins\", \"latitude\":40.585258, \"longitude\":-105.084419} \n" +
+            "    ]\n" +
+            "}";
+       // tffi.type = "submit";
+        console.log(tffidoc);
+
+        this.props.updateTffiObject(tffidoc);
     }
 
     uploadFile(event){
@@ -27,24 +45,12 @@ class UploadTffi extends Component{
             if (file) {
 
 
-                let result = reader.result;
-                // let content = "{\n" +
-                //     " \"type\" : \"trip\",\n" +
-                //      " \"version\" : 2,\n" +
-                //      " \"title\" : \"Shopping loop\",\n" +
-                //      " \"options\" : {\n" +
-                //      "    \"units\":\"miles\"\n" +
-                //      "    },\n" +
-                //      " \"places\" : [\n" +
-                //      "    {\"id\":\"dnvr\", \"name\":\"Denver\", \"latitude\":39.7392, \"longitude\":-104.9903}, \n" +
-                //      "    {\"id\":\"bldr\", \"name\":\"Boulder\", \"latitude\":40.01499, \"longitude\":-105.27055}, \n" +
-                //      "    {\"id\":\"foco\", \"name\":\"Fort Collins\", \"latitude\":40.585258, \"longitude\":-105.084419} \n" +
-                //      "    ]\n" +
-                //      "}";
+                let result = reader.result;  //makes the json string store that some where.
+
                 console.log("I before content");
-                console.log("This is real!!");
                 console.log(result);
                 console.log("I after content");
+            //   tffi(result);
 
             }
         }
