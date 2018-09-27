1d43ab615a999183d397e247c75b536795fbe52f
davidjump <unstuck.git@davidjump.com>
2018-09-26 20:57:34 -0600

    Table shows when have data and place holder for data we don't have. Refactored to try pull some code out into functions

--- client/src/components/Application/Itinerary.js
+++ client/src/components/Application/Itinerary.js
@@ -7,61 +7,63 @@ import {Container, Table, Button, Form, FormGroup, Label, Input, FormText } from
 class Itinerary extends Component{
   constructor(props) {
     super(props);
-    this.calcTotalDistance = this.calcTotalDistance.bind(this);
+    this.getRows = this.getRows.bind(this);
+    this.getDistance = this.getDistance.bind(this);
+    this.getTotalDistance = this.getTotalDistance.bind(this);
+    this.getBlankPlaceHolder= this.getBlankPlaceHolder.bind(this);
 
   }
-  calcTotalDistance  () {
+   getBlankPlaceHolder()
+  {
+    return '-';
 
   }
-
-
-  render() {
-    if( this.props.trip.places === null || typeof this.props.trip.places === 'undefined'  || this.props.trip.places.length==0)
-    {
-      console.log("place not there");
-      return (<Container></Container>);
-    }
+  getDistance (index)
+  {
     if( this.props.trip.distances === null || typeof this.props.trip.distances === 'undefined'  || this.props.trip.distances.length==0)
     {
-      console.log("distance not there");
-      return (<Container></Container>);
-
+      return this.getBlankPlaceHolder();
     }
-  //   const buttons = this.props.config.units.map((unit) =>
-  //       <Button
-  //   key={'distance_button_' + unit}
-  //   className='btn-outline-dark unit-button'
-  //   active={this.props.options.units === unit}
-  //   value={unit}
-  //   onClick={(event) => this.props.updateOptions('units', event.target.value)}
-  // >
-  //   {unit.charAt(0).toUpperCase() + unit.slice(1)}
-  // </Button>
-  // );
+    return this.props.trip.distances[index];
+
+  }
+  getTotalDistance(previousDistance,index)
+  {
+    if( this.props.trip.distances === null || typeof this.props.trip.distances === 'undefined'  || this.props.trip.distances.length==0)
+    {
+      return this.getBlankPlaceHolder();
+    }
+    return previousDistance+this.props.trip.distances[index];
+
+  }
+
+  getRows ()
+  {
     var index = -1;
     var totaldistance = 0;
 
-   const rows = this.props.trip.places.map((place)=> {
-     console.log("Distance" + this.props.trip.distances[index]);
-     if(index == -1)
-     {
-       index++;
-       return (
-           <tr key={'intinerary-row 0'}>
-           < td > {place.name} </td>
-     < td > 0 < /td>
-     < td > 0 < /td>
-     < /tr>
-     )
-
-     }
-     totaldistance = totaldistance + this.props.trip.distances[index];
-      var row = (
-          <tr key={'intinerary-row '+(index+1)}>
-          < td > {place.name} </td>
-       < td > {this.props.trip.distances[index]} < /td>
-    < td > {totaldistance}< /td>
+    const rows = this.props.trip.places.map((place)=> {
+      //console.log("Distance" + this.props.trip.distances[index]);
+      if(index == -1)
+    {
+      index++;
+      return (
+          <tr key={'intinerary-row 0'}>
+        < td > {place.name} </td>
+    < td > 0 < /td>
+    < td > 0 < /td>
     < /tr>
+    )
+
+    }
+    totaldistance = this.getTotalDistance(totaldistance,index);
+
+    var row = (
+        <tr key={'intinerary-row '+(index+1)}>
+      < td > {place.name} </td>
+  < td > {this.getDistance(index)} < /td>
+  < td > {totaldistance}< /td>
+  < /tr>
   )
     //{this.props.trip.distances[index]}
     index++;
@@ -70,23 +72,43 @@ class Itinerary extends Component{
 
 
   );
-   console.log("rows");
-   console.log(rows);
+    console.log("rows");
+    console.log(rows);
 
-    totaldistance = totaldistance+this.props.trip.distances[index];
+    totaldistance = this.getTotalDistance(totaldistance,index);
 
     rows.push(<tr key ={'intererary-row74'}>
-    < td > {this.props.trip.places[0].name} </td>
-    < td > {this.props.trip.distances[index]} < /td>
-    < td > {totaldistance}< /td>
-    < /tr>);
+      < td > {this.props.trip.places[0].name} </td>
+  < td > {this.getDistance(index)} < /td>
+  < td > {totaldistance}< /td>
+  < /tr>);
+    return rows;
+
+  }
+
+  render() {
+    let rows;
+    if( this.props.trip.places === null || typeof this.props.trip.places === 'undefined'  || this.props.trip.places.length==0)
+    {
+      console.log("place not there");
+      return (<Container></Container>);
+    }
+    // if( this.props.trip.distances === null || typeof this.props.trip.distances === 'undefined'  || this.props.trip.distances.length==0)
+    // {
+    //   return (<Container></Container>);
+    //
+    // }
+
+      rows = this.getRows();
+
+
 
     return (
         <Table>
         <thead>
         <tr>
     <th>Destination</th>
-    <th>Leg Distance</th>
+    <th>Leg of the Trip Distance</th>
     <th>Total Trip Distance</th>
     </tr>
     </thead>
