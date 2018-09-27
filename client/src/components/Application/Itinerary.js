91074696ddeaab63e692f23973c940b4f43dbe72
davidjump <unstuck.git@davidjump.com>
2018-09-26 19:10:52 -0600

    Works for city need add the return path

--- client/src/components/Application/Itinerary.js
+++ client/src/components/Application/Itinerary.js
@@ -1,17 +1,32 @@
 import React, {Component} from 'react'
 import {Card, CardHeader, CardBody, ButtonGroup} from 'reactstrap'
-import { Table, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap'
+import {Container, Table, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap'
 
 
 
 class Itinerary extends Component{
   constructor(props) {
     super(props);
+    this.calcTotalDistance = this.calcTotalDistance.bind(this);
+
+  }
+  calcTotalDistance  () {
 
   }
 
 
   render() {
+    if( this.props.trip.places === null || typeof this.props.trip.places === 'undefined'  || this.props.trip.places.length==0)
+    {
+      console.log("place not there");
+      return (<Container></Container>);
+    }
+    if( this.props.trip.distances === null || typeof this.props.trip.distances === 'undefined'  || this.props.trip.distances.length==0)
+    {
+      console.log("distance not there");
+      return (<Container></Container>);
+
+    }
   //   const buttons = this.props.config.units.map((unit) =>
   //       <Button
   //   key={'distance_button_' + unit}
@@ -23,21 +38,48 @@ class Itinerary extends Component{
   //   {unit.charAt(0).toUpperCase() + unit.slice(1)}
   // </Button>
   // );
+    var index = -1;
+    var totaldistance = 0;
 
    const rows = this.props.trip.places.map((place)=> {
+     console.log("Distance" + this.props.trip.distances[index]);
+     if(index == -1)
+     {
+       index++;
+       return (
+           <tr key={'intinerary-row 0'}>
+           < td > {place.name} </td>
+     < td > 0 < /td>
+     < td > 0 < /td>
+     < /tr>
+     )
 
+     }
+     totaldistance = totaldistance + this.props.trip.distances[index];
       var row = (
-          <tr>
+          <tr key={'intinerary-row '+(index+1)}>
           < td > {place.name} </td>
-       < td > Otto < /td>
-    < td > foo < /td>
+       < td > {this.props.trip.distances[index]} < /td>
+    < td > {totaldistance}< /td>
     < /tr>
   )
+    //{this.props.trip.distances[index]}
+    index++;
     return row;
   }
 
+
   );
-//    console.log(rows);
+   console.log("rows");
+   console.log(rows);
+
+
+
+//     rows.push(<tr>
+//     < td > {this.props.trip.places[0]} </td>
+//     < td > {this.props.trip.distances[0]} < /td>
+//     < td > {totaldistance+this.props.trip.distances[0]}< /td>
+//     < /tr>);
 
     return (
         <Table>
@@ -45,7 +87,7 @@ class Itinerary extends Component{
         <tr>
     <th>Destination</th>
     <th>Leg Distance</th>
-    <th>Total Trip Distan  ce</th>
+    <th>Total Trip Distance</th>
     </tr>
     </thead>
     <tbody>
