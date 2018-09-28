2d5904a0367c7e760b92e16c054625113a63b02c
alaswell <alexlaswell@gmail.com>
2018-09-27 18:42:27 -0600

    This works to log props finally, but is still not displaying the map correctly

--- client/src/components/Application/Map.js
+++ client/src/components/Application/Map.js
@@ -1,25 +1,25 @@
 import React, { Component } from 'react'
-import { Card, CardHeader, CardBody } from 'reactstrap'
-import { Document } from 'reactstrap'
+import { Card, CardBody, Media} from 'reactstrap'
 import { get_comfig } from '../../api/api.js'
 
 /* The Map allows the user to view the map image provided in the Trip TFFI file */
 class Map extends Component{
     constructor(props) {
         super(props);
+      this.setMap = this.setMap.bind(this);
     }
 
-    setMapSource() {
-        document.getElementById("mapImage").src = this.props.trip.map;
+    setMap(props) {
+        console.log(this)
+        console.log(this.props);
     }
-    
+
     render() {
         return (
             <Card>
-                <CardBody>
-                    <img id="mapImage" src="" />
-                    {this.setMapSource()}
-                </CardBody>
+              <CardBody>
+                <img src='data:image/svg+xml;utf8,<svg xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" version="1.0" width="1066.6073" height="783.0824" id="svg2338"> <circle cx="50" cy="50" r="40" stroke="green" stroke-width="4" fill="yellow"</svg>'/>
+              </CardBody>
             </Card>
         )
     }
