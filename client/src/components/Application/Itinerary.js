a62981a57286412a633217924f3a3ff627a2adec
alaswell <alexlaswell@gmail.com>
2018-10-12 17:36:49 -0600

    Clean code requested changes

--- client/src/components/Application/Itinerary.js
+++ client/src/components/Application/Itinerary.js
@@ -8,97 +8,105 @@ class Itinerary extends Component{
     super(props);
     this.getRows = this.getRows.bind(this);
     this.getDistance = this.getDistance.bind(this);
+    this.getFirstRow = this.getFirstRow.bind(this);
+    this.getNthRow = this.getNthRow.bind(this);
     this.getTotalDistance = this.getTotalDistance.bind(this);
-    this.getBlankPlaceHolder= this.getBlankPlaceHolder.bind(this);
+    this.returnBlankPlaceHolder= this.returnBlankPlaceHolder.bind(this);
 
   }
+
 
   //Function returns the placeholder if no data can be filled in the table.
-   getBlankPlaceHolder() {
-    return '-';
+  returnBlankPlaceHolder() {
+    if( this.props.trip.distances === null || typeof this.props.trip.distances === 'undefined'  || this.props.trip.distances.length==0) {
+      return true;
+    }
   }
+
+
   //Get the distance out of the TIFF obj. If no distance value is present then returns placeholder
   getDistance (index) {
-    if( this.props.trip.distances === null || typeof this.props.trip.distances === 'undefined'  || this.props.trip.distances.length==0)
-    {
-      return this.getBlankPlaceHolder();
+    if( this.returnBlankPlaceHolder()) {
+      return '-';
     }
     return this.props.trip.distances[index];
-
   }
+
+
+
+  /**
+   * Build any the first row in the table
+   */
+  getFirstRow (name) {
+    //table is |place name| leg distance| total distance|
+    return ( <tr key={'intinerary-row 0'}>
+      < td > {name} </td>
+      < td > 0 </td>
+      < td > 0 </td>
+    </tr> );
+  }
+
+
+  /**
+   * Build any Nth row in the table
+   */
+  getNthRow (index, name, totalDistance) {
+    return ( <tr key={'intinerary-row '+(index+1)}>
+      < td > {name} </td>
+      < td > {this.getDistance(index)} </td>
+      < td > {totalDistance}</td>
+    </tr> )
+  }
+
 
   /**
    * Calc totalDistance from previousDestance and the index of distance in the distance array
    */
-
-  getTotalDistance(previousDistance,index) {
-    if( this.props.trip.distances === null || typeof this.props.trip.distances === 'undefined'  || this.props.trip.distances.length==0)
-    {
-      return this.getBlankPlaceHolder();
+  getTotalDistance (previousDistance,index) {
+    if( this.returnBlankPlaceHolder()) {
+      return '-';
     }
     return previousDistance+this.props.trip.distances[index];
-
   }
+
 
   /**
    * Builds the row of table to present in the view
    */
   getRows () {
     var index = -1;  //starts at -1 to indicate that the foreach is on the 1 place and
-                    // total distance and leg distance are both 0
+    // total distance and leg distance are both 0
     var totaldistance = 0;
 
     const rows = this.props.trip.places.map((place)=> {
-
       if(index == -1){ // the first entry in the table and is special because leg and total at both 0
-
-      index++;
-
+        index++;
+        return this.getFirstRow (place.name);
+      }
+      totaldistance = this.getTotalDistance(totaldistance,index);
       //table is |place name| leg distance| total distance|
-      return (
-          <tr key={'intinerary-row 0'}>
-            < td > {place.name} </td>
-            < td > 0 < /td>
-            < td > 0 < /td>
-          < /tr>
-      )
-
-    }
-    totaldistance = this.getTotalDistance(totaldistance,index);
-    //table is |place name| leg distance| total distance|
-    //table entry for middle destinations
-    var row = (
-        <tr key={'intinerary-row '+(index+1)}>
-          < td > {place.name} </td>
-          < td > {this.getDistance(index)} < /td>
-          < td > {totaldistance}< /td>
-        < /tr>
-  )
-    index++;
-    return row;
-  }
-
-
-  );
+      //table entry for middle destinations
+      var row = ( this.getNthRow(index, place.name, totaldistance) );
+      index++;
+      return row;
+    });
 
     totaldistance = this.getTotalDistance(totaldistance,index);
 
     rows.push(
-            <tr key ={'intererary-row74'}>
-              < td > {this.props.trip.places[0].name} </td>
-              < td > {this.getDistance(index)} < /td>
-              < td > {totaldistance}< /td>
-             < /tr>);
+        <tr key ={'intererary-row74'}>
+          < td > {this.props.trip.places[0].name} </td>
+          < td > {this.getDistance(index)} </td>
+          < td > {totaldistance}</td>
+        </tr>);
     return rows;
-
   }
+
 
   /**
    *   Renders the view
    */
   render() {
-    //Checks to see if places has been set.
-
     if( this.props.trip.places === null || typeof this.props.trip.places === 'undefined'  || this.props.trip.places.length==0)
     {
       return (<Container></Container>);  //return nothing because there is not places to build a table from
@@ -108,20 +116,20 @@ class Itinerary extends Component{
         <Card>
           <CardBody>
             <Table>
-            <thead>
+              <thead>
               <tr>
-                  <th>Destination</th>
-                  <th>Leg of the Trip Distance</th>
-                  <th>Total Trip Distance</th>
+                <th>Destination</th>
+                <th>Leg of the Trip Distance</th>
+                <th>Total Trip Distance</th>
               </tr>
-            </thead>
-            <tbody>
-               {this.getRows()}
-             </tbody>
-        </Table>
-      </CardBody>
-    </Card>
-  )
+              </thead>
+              <tbody>
+              {this.getRows()}
+              </tbody>
+            </Table>
+          </CardBody>
+        </Card>
+    )
   }
 }
 
