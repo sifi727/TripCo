1bd031c6419617f5a1b72bbf392cf4c30d573f00
sifi727 <jsiler727@outlook.com>
2018-10-26 17:59:58 -0600

    Created Search.js and modified a few lines in the Application.js: line#11- Added an import 'Search' line#24- Updated the version from '3' to '4' line#218- Added '<Search/>' in the Container tag.

---
+++ client/src/components/Application/Search.js
@@ -0,0 +1,23 @@
+import React, {Component} from 'react'
+import {Button, Card, CardBody, CardTitle, InputGroup, InputGroupText} from 'reactstrap'
+import {request, get_comfig} from '../../api/api.js'
+
+class Search extends Component {
+        constructor(props) {
+            super(props);
+        }
+
+
+    render()
+    {
+        return (
+            <Card>
+                <CardBody>
+                    Testing.......
+                </CardBody>
+            </Card>
+        )
+    }
+}
+
+export default Search;
