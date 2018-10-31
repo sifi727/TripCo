87f3120a9f935034d2dd252cb13e128eda7a526d
sifi727 <jsiler727@outlook.com>
2018-10-31 09:49:25 -0600

    Added more to the 'Search.test.js'.

--- client/test/Search.test.js
+++ client/test/Search.test.js
@@ -2,17 +2,47 @@ import './enzyme.config.js'                   // (1)
 import React from 'react'
 import { shallow } from 'enzyme'
 import Search from '../src/components/Application/Search'
+import {Col, FormText, Input} from "reactstrap";
 
 const  startProps = {
     port: location.port,
-    hostname: location.hostname,
+    hostname: location.hostname
 };
 
-function testTextField() {
-
-
-    const wrapper = shallow((<Search />));
+function testTextFieldExist() {
+    const wrapper = shallow(( <Search port={startProps.port} hostname={startProps.hostname}/> ));
     expect(wrapper.exists('#SearchField')).toEqual(true);
 }
 
-test('Checks to see that the search text field renders',testTextField());
+function testNumberFieldExist() {
+    const wrapper = shallow((<Search port={startProps.port} hostname={startProps.hostname}/>));
+    expect(wrapper.exists('#SearchLimitField')).toEqual(true);
+}
+
+function testButtonExist() {
+    const wrapper = shallow(( <Search port={startProps.port} hostname={startProps.hostname}/> ));
+    expect(wrapper.exists('#SearchButtonId')).to.equal(true);
+}
+
+function testTextField() {
+    const searchProps = {
+        search:{
+            "version"   : 4,
+            "type"      : "search",
+            "match"     : "",
+            "filters"   : [],
+            "limit"     : 0,
+            "found"     : 0,
+            "places"    : []
+        }
+    }
+
+    const wrapper = shallow(( <Search port={startProps.port} hostname={startProps.hostname}  />));
+    expect(wrapper.state().search.match).toEqual("");
+
+}
+
+test('Checks to see that the search text field renders',testTextFieldExist());
+test('Checks to see that the search number field renders',testNumberFieldExist());
+test('Checks to see that the search button renders',testButtonExist());
+test('Checks to see that the search text field changes the \"match\" field in the search state',testTextField());
