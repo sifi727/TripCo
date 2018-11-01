1ce9846c435d4ccae08188d95845f57cc8cbf6eb
sifi727 <jsiler727@outlook.com>
2018-10-31 20:53:41 -0600

    Finished writing a few tests for 'Search.test.js'.  The tests are passing, but they are being ignored (This is most likely due to the InTelliJ/Maven version that we downloaded according to Andy, the TA. Apparently it is an issue that a lot of students are currently running into).

--- client/test/Search.test.js
+++ client/test/Search.test.js
@@ -2,7 +2,6 @@ import './enzyme.config.js'                   // (1)
 import React from 'react'
 import { shallow } from 'enzyme'
 import Search from '../src/components/Application/Search'
-import {Col, FormText, Input} from "reactstrap";
 
 const  startProps = {
     port: location.port,
@@ -19,30 +18,35 @@ function testNumberFieldExist() {
     expect(wrapper.exists('#SearchLimitField')).toEqual(true);
 }
 
-function testButtonExist() {
+function testSearchButtonExist() {
     const wrapper = shallow(( <Search port={startProps.port} hostname={startProps.hostname}/> ));
-    expect(wrapper.exists('#SearchButtonId')).to.equal(true);
+    expect(wrapper.exists('#SearchButtonId')).toEqual(true);
 }
 
-function testTextField() {
-    const searchProps = {
-        search:{
-            "version"   : 4,
-            "type"      : "search",
-            "match"     : "",
-            "filters"   : [],
-            "limit"     : 0,
-            "found"     : 0,
-            "places"    : []
-        }
-    }
-
+function testTextFieldOnChange() {
     const wrapper = shallow(( <Search port={startProps.port} hostname={startProps.hostname}  />));
     expect(wrapper.state().search.match).toEqual("");
+    wrapper.find('#SearchField').simulate('change', {
+        target: {
+            value:"Den"
+        }
+    });
+    expect(wrapper.state().search.match).toEqual("Den");
+}
 
+function testNumberFieldOnChange() {
+    const wrapper = shallow(( <Search port={startProps.port} hostname={startProps.hostname}  />));
+    expect(wrapper.state().search.limit).toEqual(0);
+    wrapper.find('#SearchField').simulate('change', {
+        target: {
+            value:7
+        }
+    });
+    expect(wrapper.state().search.match).toEqual(7);
 }
 
 test('Checks to see that the search text field renders',testTextFieldExist());
 test('Checks to see that the search number field renders',testNumberFieldExist());
-test('Checks to see that the search button renders',testButtonExist());
-test('Checks to see that the search text field changes the \"match\" field in the search state',testTextField());
+test('Checks to see that the search button renders',testSearchButtonExist());
+test('Checks to see that the search text field changes the \"match\" field in the search state',testTextFieldOnChange());
+test('Checks to see that the search number field changes the \"limit\" field in the search state',testNumberFieldOnChange());
