084731c83b96b47d22201c5fbca004a927822728
davidjump <unstuck.git@davidjump.com>
2018-11-28 11:39:33 -0700

    updated config hostname

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -11,8 +11,8 @@ export async function request(body, type, port=get_port(), host=location.hostnam
   }).then(response => {return response.json()}).catch(err => {console.error(err)});
 }
 
-export async function get_config(type, port=get_port()) {
-  return fetch('http://' + location.hostname + ":" + port + '/config', {
+export async function get_config(type, port=get_port(), host=location.hostname) {
+  return fetch('http://' + host + ":" + port + '/config', {
     method:"GET"
   }).then(response => {return response.json()}).catch(err => {console.error(err)});
 }
